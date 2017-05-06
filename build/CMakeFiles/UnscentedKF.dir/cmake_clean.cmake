c18e511d10e5e9585f080c857f5f0e678c2a0e28
Kofi Baafi <kbaafi@gmail.com>
2017-05-06 14:14:01 +0300

    build

---
+++ build/CMakeFiles/UnscentedKF.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UnscentedKF.dir/home/kbaafi/SDCAR/CarND-Unscented-Kalman-Filter-Project/src/ukf.cpp.o"
+  "CMakeFiles/UnscentedKF.dir/home/kbaafi/SDCAR/CarND-Unscented-Kalman-Filter-Project/src/main.cpp.o"
+  "CMakeFiles/UnscentedKF.dir/home/kbaafi/SDCAR/CarND-Unscented-Kalman-Filter-Project/src/tools.cpp.o"
+  "UnscentedKF.pdb"
+  "UnscentedKF"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UnscentedKF.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
